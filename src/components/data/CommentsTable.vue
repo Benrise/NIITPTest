1800bafdb11e5831847f0718c37877a9c13414bf
Benrise <benrise_x@mail.ru>
2023-08-17 11:20:32 +0300

    Visual icon status added to completed field. Header added in comments table.

--- src/components/data/CommentsTable.vue
+++ src/components/data/CommentsTable.vue
@@ -4,10 +4,32 @@
             :value="comments" 
             tableStyle="min-width: 50rem"
             class="p-datatable-sm"
+            v-model:filters="filters"
+            dataKey="id"
+            :loading="loading"
         >
-            <Column field="id" header="ID" style="width: 25%"></Column>
-            <Column field="title" header="Содержание" style="width: 25%"></Column>
-            <Column field="completed" header="Статус" style="width: 25%"></Column>
+            <template #header>
+                <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+                    <span class="text-xl text-900 font-bold">Комментарии</span>
+                    <span class="p-input-icon-left">
+                        <i class="pi pi-search" />
+                        <InputText v-model="filters.global.value" placeholder="Поиск по комментариям" />
+                    </span>
+                </div>
+            </template>
+            <template #empty> Пользователи не найдены. </template>
+            <template #loading> Загрузка данных о пользователях. Пожалуйста, подождите. </template>
+            <Column field="id" header="ID" style="width: 5%"></Column>
+            <Column field="title" header="Содержание" style="width: 90%"></Column>
+            <Column field="completed" dataType="boolean" header="Статус" style="width: 5%">
+                <template #body="{ data }">
+                    <i class="pi" :class="{ 'pi-check-circle text-green-500': data.completed, 'pi-times-circle text-red-400': !data.completed }"></i>
+                </template>
+                <template #filter="{ filterModel, filterCallback }">
+                    <TriStateCheckbox v-model="filterModel.value" @change="filterCallback()" />
+                </template>
+            </Column>
+            <template #footer> Всего {{ comments ? comments.length : 0 }} комменнтариев. </template>
         </DataTable>
     </div>
 </template>
@@ -15,17 +37,24 @@
 <script>
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import { FilterMatchMode } from "primevue/api";
 import Axios from 'axios';
 
     export default { 
-        components: {Axios, DataTable, Column},
+        components: {DataTable, Column, Axios, Button, InputText, FilterMatchMode},
         mounted(){
             this.getComments()
         },
         data() {
             return{
                 comments: {},
+                filters: {
+                    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+                    status: { value: null, matchMode: FilterMatchMode.EQUALS }
+                    
+            },
             }
         },
         methods: {
