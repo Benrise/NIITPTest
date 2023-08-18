5035f9f98e0ce5245e1dd0adf71672e2935310ce
Benrise <benrise_x@mail.ru>
2023-08-18 15:32:09 +0300

    Extra npm package removed

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,10 @@ import '@/scss/style.scss'
 import App from '@/App.vue'
 import router from "@/router/router";
 import PrimeVue from 'primevue/config';
-import VueMobileDetection from "vue-mobile-detection";
 
 const app = createApp(App)
 
 app
     .use(router)
     .use(PrimeVue)
-    .use(VueMobileDetection)
     .mount('#app')
